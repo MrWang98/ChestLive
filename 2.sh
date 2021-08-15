b88bcc29b5b4d5026994a049e6212b8907a93bf2
MrWang98 <62923785+MrWang98@users.noreply.github.com>
2021-08-15 14:26:50 +0800

    Delete 2.sh

--- 2.sh
+++
@@ -1,7 +0,0 @@
-`rm -rf Set2/test/*`
-$(cp -r process_data/angle/$1/* Set2/test/)
-$(cp -r process_data/distance/$1/* Set2/test/)
-$(cp -r process_data/noise/$1/* Set2/test/)
-$(cp -r process_data/pose/$1/* Set2/test/)
-$(cp -r process_data/devices/$1* Set2/test/)
-$(cp -r process_data/wakeword/$1* Set2/test/)
